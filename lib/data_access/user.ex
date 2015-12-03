67d961cc650fa29a913f46ff91395d8bc4e91383
mikkel <desmaagraa@gmail.com>
2015-12-03 13:37:28 +0100

    add get all users

--- lib/data_access/user.ex
+++ lib/data_access/user.ex
@@ -1,3 +1,16 @@
 defmodule Magpie.DataAccess.User do
-  
+  import Magpie.DataAccess.Util
+  require User
+
+  def get do
+    {:ok, client} = :cqerl.new_client()
+    {:ok, result} = :cqerl.run_query(client, "SELECT * FROM magpie.users")
+
+    users = :cqerl.all_rows(result)
+
+    Enum.map(users, &to_user/1)
+end
+
+defp to_user(u) do
+  [id: :uuid.uuid:to:string(u[:id]), username: u[:username], password: u[:password], mail: u[:mail], admin: u[:admin]]
 end
