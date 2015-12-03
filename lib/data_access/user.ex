8d4707448212bf4055b538e37be786bfe6347360
mikkel <desmaagraa@gmail.com>
2015-12-03 14:01:56 +0100

    add get with email

--- lib/data_access/user.ex
+++ lib/data_access/user.ex
@@ -8,9 +8,24 @@ defmodule Magpie.DataAccess.User do
     users = :cqerl.all_rows(result)
 
     Enum.map(users, &to_user/1)
-end
+  end
+
+  def get(email) do
+    {:ok, client} = new_client()
+
+    query = cql:query(
+      statement: "SELECT email, username, admin, password FROM magpie.users WHERE id = ?;",
+      values: [email: email])
+    {:ok, result} = :cqerl.run_query(client, query)
+    case :cqerl.next(result) do
+      {row, next_result} ->
+        :cqerl.close_client(client)
+        {:ok, to_user(row)}
+      :empty_dataset -> {:error, "Not found"}
+    end
+  end
 
   defp to_user(u) do
-    [username: u[:username], password: u[:password], mail: u[:mail], admin: u[:admin]]
+    [username: u[:username], password: u[:password], email: u[:email], admin: u[:admin]]
   end
 end
